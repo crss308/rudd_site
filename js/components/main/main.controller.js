de8d63d4a5846673d61cd20dce1777ae61d5d3e4
matthew Rudd <matthew@matthews-MacBook-Pro-2.local>
2018-10-11 15:33:13 -0600

    soccer_ball.jpg

--- js/components/main/main.controller.js
+++ js/components/main/main.controller.js
@@ -10,7 +10,7 @@
 
   function mainController($scope) {
     /*jshint validthis: true */
-    this.greeting = 'Welcome to Allen Fordham.com! This page is currently used for testing';
+    this.greeting = 'Welcome to my site!! This page is currently used for testing';
 
 
     // simplest http script using jquery - ping a site using $http.get, then print the results, whatever that is. It could just as easily say "if (result) == dispense, then drop"
