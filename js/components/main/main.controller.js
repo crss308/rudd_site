a7291c4cae8d9c334b7f556ec61f5c0b6f1d8b30
matthew Rudd <matthew@matthews-MacBook-Pro-2.local>
2018-10-11 14:46:37 -0600

    git init

---
+++ js/components/main/main.controller.js
@@ -0,0 +1,22 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('myApp.components.main', [])
+    .controller('mainController', mainController);
+
+  mainController.$inject = ['$scope', '$http'];
+
+  function mainController($scope) {
+    /*jshint validthis: true */
+    this.greeting = 'Welcome to Allen Fordham.com! This page is currently used for testing';
+
+
+    // simplest http script using jquery - ping a site using $http.get, then print the results, whatever that is. It could just as easily say "if (result) == dispense, then drop"
+  //   $http.get('http://google.com')
+  //     .then(result => console.log(result))
+
+  }
+
+})();
